3d75871c9382d04c8fbcb8a300cbf13159af5ac8
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-09-28 15:22:03 +0330

    update
    we can get user form values.
    oop needs to be worked on.

--- script.js
+++ script.js
@@ -21,10 +21,63 @@ const shamsi = date
 const currentYear = shamsi;
 const minYear = currentYear - 20;
 
-for (let i = currentYear ; i >= minYear; i--) {
+for (let i = currentYear; i >= minYear; i--) {
   const option = document.createElement("option");
   option.textContent = i;
   document.querySelector("#years").appendChild(option);
 }
+
+//clicking on the btn.....................................
+const generatorBtn = document.querySelector("#generator");
+generatorBtn.addEventListener('click', ()=>{
+  // car model value
+  const chosenModel = document.querySelector("#cars > option").value;
+
+  // car year value
+  const chosenYear = document.querySelector("#years > option").value;
+  
+  // insurance type value
+  let chosenInsuranceT = 0;
+  const basic = document.querySelector('#basic');
+  const complete = document.querySelector('#complete');
+  // check only one checkbox & get insuranceRatio
+
+ const checkboxes = document.querySelectorAll('.checkbox');
+ checkboxes.forEach((checkbox)=>{
+    checkbox.addEventListener('change', ()=>{
+        if (basic.checked) {
+            chosenInsuranceT = 30;
+            complete.checked = false ;
+              // ************************************* 
+       } else if (complete.checked) {
+            chosenInsuranceT =  50;
+            basic.checked = false ;
+           }
+     })
+ })
+
+ return [chosenModel, chosenYear, chosenInsuranceT]
+
+})
+
+
+// calculate with OOP......................................
+class Quote {
+  constructor(model, year, insuranceType) {
+    this.model;
+    this.year;
+    this.insuranceType;
+  }
+
+test(){
+console.log(`car model is ${this.model}, car year is ${this.year} and insurance type is ${this.insuranceType}.`);
+}
+}
+
+const user = new Quote(chosenModel, chosenYear, chosenInsuranceT);
+
+user.test();
+
+
 
 
